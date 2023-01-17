5b0f9a8897b3743144f9a6479e38ab322a63b09f
Thomas Klein <thomas.klein@jwalab.com>
2023-01-17 12:04:01 +0100

    reference aws policy with ARN

--- profile.tf
+++ profile.tf
@@ -22,11 +22,7 @@ resource "aws_iam_instance_profile" "bastion_profile" {
   role = aws_iam_role.bastion_role.name
 }
 
-data "aws_iam_policy" "ssm_managed_instance_core" {
-  name = "AmazonSSMManagedInstanceCore"
-}
-
 resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core_policy_attachment" {
-  policy_arn = data.aws_iam_policy.ssm_managed_instance_core.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role = aws_iam_role.bastion_role.name
 }
